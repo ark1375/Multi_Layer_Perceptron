2a030132e0663614d90e0a22279730496228b326
ark1375 <ark1375@hotmail.com>
2020-07-09 05:30:34 +0430

    MLP with sigmoid class as Activation function

---
+++ OperationCharmander/src/main/java/MLP/Sig_MLP.java
@@ -0,0 +1,12 @@
+
+package MLP;
+
+public class Sig_MLP extends MLP{
+    
+    @Override
+    public double activation_function(double input){return 2.0;}
+    
+    @Override
+    public double activation_function_derivative(double input){return 2.0;}
+    
+}
