8a5494c44e63f7e2a09b3b053239c0e5158e00fb
ark1375 <ark1375@hotmail.com>
2020-07-09 05:31:25 +0430

    Setters and abstraction of activation function

--- OperationCharmander/src/main/java/MLP/MLP.java
+++ OperationCharmander/src/main/java/MLP/MLP.java
@@ -1,12 +1,12 @@
 
 package MLP;
-import java.util.Vector;
 import java.util.ArrayList;
+import java.lang.Math;
 
 public abstract class MLP {
     
     private String mlpName;             //A chosen name for the ann
-    private ArrayList<Vector> data;     //Actual input data that ann will be train with
+    private ArrayList<float[]> data;     //Actual input data that ann will be train with
     private int hiddenLayerCount;       //Number of hidden layers
     private int inputLayerCount;        //Number of input layers which is actualy the dimension of the input
     private int outputLayerCount;       //Number of output layers whic
@@ -14,8 +14,15 @@ public abstract class MLP {
     private boolean isClassActivated = false;
     
     MLP(){}
+    
     MLP(String name , int hiddenLayer , int outputLayer){
         
+        setMlpName(mlpName);
+        setHiddenLayerCount(hiddenLayerCount);
+        setInputLayerCount(inputLayerCount);
+        setOutputLayerCount(outputLayerCount);
+        
+        this.isClassActivated = true;
     }
 
     public void setMlpName(String mlpName) {
@@ -23,16 +30,25 @@ public abstract class MLP {
     }
 
     public void setHiddenLayerCount(int hiddenLayerCount) {
-        this.hiddenLayerCount = hiddenLayerCount;
+        if (hiddenLayerCount >= 0)
+            this.hiddenLayerCount = hiddenLayerCount;
     }
 
     public void setInputLayerCount(int inputLayerCount) {
-        this.inputLayerCount = inputLayerCount;
+             
+        if (inputLayerCount >= 1)
+            this.inputLayerCount = inputLayerCount;
     }
 
     public void setOutputLayerCount(int outputLayerCount) {
-        this.outputLayerCount = outputLayerCount;
+        if (this.outputLayerCount >= 1)
+            this.outputLayerCount = outputLayerCount;
     }
+    
+    public boolean activate(){return false;}         //not completed yet
+    
+    public abstract double activation_function();
+    
     
     
     
