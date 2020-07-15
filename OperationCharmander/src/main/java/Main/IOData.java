a4de284fe587fbe55a05167d7d713c5163c43b13
hadinajafi <hadinajafi.77@gmail.com>
2020-07-16 00:52:05 +0430

    Preprocessing on image. TODO: dynamic path for reading all images.

--- OperationCharmander/src/main/java/Main/IOData.java
+++ OperationCharmander/src/main/java/Main/IOData.java
@@ -3,9 +3,6 @@ package Main;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 /*
@@ -13,24 +10,49 @@ import javax.imageio.ImageIO;
  */
 public class IOData {
 
-    private String path = "src/main/java/Resources/2.png";  //TODO: get dynamic path
-    private int[] rgb;  //the rgb code for one pixel
+    private String path = "src/main/java/Resources/";  //TODO: get dynamic path
+    private String fileName = "2.png";
+    private BufferedImage image;
     private int imageWidth = 28;    //image width, by default is 28 pixels
     private int imageHeight = 28;   //image height, by default is 28 pixels
+    private double [][] binaryImage;   //image built from the actual image but the pixel color is binary, 0 means black, 1 means white.
 
     public IOData() {
-        rgb = new int[4];
-        BufferedImage image2 = readImage();
-        System.out.println(Arrays.toString(image2.getRaster().getPixel(12, 11, rgb)));
-        System.err.println("Done");
+        binaryImage = new double[imageWidth][imageHeight];
+        preProcessing();
     }
     
-    private void preProcessing(int [][] image){
-        
+    /**
+     * first: read the image
+     * then for every pixel in the image, gather rgb data,
+     * then convert rgb data to binary data, just black and white.
+     */
+    private void preProcessing(){
+        for (int i = 0; i < imageHeight; i++){
+            for(int j = 0; j < imageWidth; j++){
+                image = readImage();    //read the image of the path and filename attribute.
+                double[] rgb = new double[4]; //the array that holds the pixel rgb raw data.
+                image.getRaster().getPixel(j, i, rgb); //get the [i][j] pixel of the image and copy the rgb meta into the rgb[] array
+                //initializing the binary matrix
+                if(rgb[0] < 160){
+                    if(rgb[1] < 160){
+                        if(rgb[2] < 160){
+                            binaryImage[i][j] = 0;
+                        }
+                        else
+                            binaryImage[i][j] = 1;
+                    }
+                    else
+                        binaryImage[i][j] = 1;
+                }
+                else
+                    binaryImage[i][j] = 1;
+            }
+        }
     }
 
     private BufferedImage readImage() {
-        File file = new File(path);
+        File file = new File(path + fileName);
         try {
             BufferedImage image = ImageIO.read(file);
             return image;
