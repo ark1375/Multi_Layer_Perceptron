55b21420d97720ecd02b07385301062e99b033d2
ark1375 <ark1375@hotmail.com>
2020-07-15 14:08:14 +0430

    dataset upload and minor changes to main and MLP class

--- OperationCharmander/src/main/java/MLP/MLP.java
+++ OperationCharmander/src/main/java/MLP/MLP.java
@@ -1,54 +1,73 @@
 
 package MLP;
 import java.util.ArrayList;
-import java.lang.Math;
-import javafx.application.Platform;
+
 
 public abstract class MLP {
     
-    private String mlpName ="";                 //A chosen name for the ann
-    private ArrayList<float[]> data = null;     //Actual input data that ann will be train with
-    private int hiddenLayerCount = -1 ;         //Number of hidden layers
-    private int inputLayerCount = 0;            //Number of input layers which is actualy the dimension of the input
-    private int outputLayerCount = 0;           //Number of output layers whic
+    private String mlpName ="";                  //A chosen name for the ann
     
-    private float[][][] weights;                  //Weights of the network
+    private int hiddenLayerCount = -1 ;          //Number of hidden layers
+    private int inputLayerCount = 0;             //Number of input layers which is actualy the dimension of the input
+    private int outputLayerCount = 0;            //Number of output layers whic
     
-    private boolean isClassActivated;
-    
-    MLP(){isClassActivated = false;}
-    
-    MLP(String name , int hiddenLayer , int outputLayer){
-        
-        setMlpName(mlpName);
-        setHiddenLayerCount(hiddenLayerCount);
-        setInputLayerCount(inputLayerCount);
-        setOutputLayerCount(outputLayerCount);
-        
-        this.isClassActivated = true;
-    }
+    private double learingingRate = 0;           //The alpha parameter or the learning reate
+    private double[][][] weights;                //Weights of the network
+    private ArrayList<double[]> data = null;     //Actual input data that ann will be train with
 
-    public void setMlpName(String mlpName) {
-        this.mlpName = mlpName;
-    }
+    private boolean isClassActivated = false;
+    
+    MLP(){}
 
-    public void setHiddenLayerCount(int hiddenLayerCount) {
+    /********************************Setters************************************/
+
+    private void setHiddenLayerCount(int hiddenLayerCount) {
         if (hiddenLayerCount >= 0)
             this.hiddenLayerCount = hiddenLayerCount;
     }
 
-    public void setInputLayerCount(int inputLayerCount) {
+    private void setInputLayerCount(int inputLayerCount) {
              
         if (inputLayerCount >= 1)
             this.inputLayerCount = inputLayerCount;
     }
 
-    public void setOutputLayerCount(int outputLayerCount) {
+    private void setOutputLayerCount(int outputLayerCount) {
         if (this.outputLayerCount >= 1)
             this.outputLayerCount = outputLayerCount;
     }
     
-    public boolean activate(){
+    
+    public void setMlpName(String mlpName) {
+        this.mlpName = mlpName;
+    }
+
+    public void setLearningRate(double lrate){
+        
+        if (lrate >= 1)
+            this.learingingRate = 1;
+        
+        else if (lrate <= 0)
+            this.learingingRate = 0.0001;
+       
+        else
+            this.learingingRate = lrate;
+            
+    }
+    
+    
+    /***************************************************************************/
+    
+    
+    //this method will initialize the network
+    // inputing number of hidden layers, train data, alpha parameter, number of hidden layers
+    public void init(){
+    
+    }
+    
+    private boolean isActivated(){
+        
+        isClassActivated = true;
         
         if (mlpName == ""){
             System.out.println("Name not Entered");
@@ -71,12 +90,14 @@ public abstract class MLP {
             this.isClassActivated = false;
         }
         
-        if (hiddenLayerCount != -1 && this.inputLayerCount != 0 && this.outputLayerCount != 0 && this.mlpName != "") 
-            this.isClassActivated = true;
+        if (this.learingingRate == 0){
+            System.out.println("Learning Rate is not entered");
+            this.isClassActivated = false;
+        }
         
         return this.isClassActivated;
         
-    }         //not completed yet
+    }   
     
     public abstract double activation_function(double input);
     
@@ -94,6 +115,15 @@ public abstract class MLP {
         
         
     } 
+
+    private double error_function(float signal , float des_signal){
+
+        return des_signal-signal;
+
+
+    }
+    
+    public void inititate_learning(int epochs){}
     
     
     
--- OperationCharmander/src/main/java/Main/Main.java
+++ OperationCharmander/src/main/java/Main/Main.java
@@ -1,12 +1,82 @@
 
 package Main;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.lang.Math;
 
+//class vector{
+        
+//        public double x = 0;
+//        public double y = 0;
+//        
+//        vector (double x , double y){
+//            this.x = x;
+//            this.y = y;
+//        }
+//        
+//        vector(){}
+//        
+//        public vector normal(){
+//            if (x == 0 && y ==0)
+//                return new vector(0,0);
+//            else {
+//                vector v = new vector();
+//                v.x = x / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+//                v.y = y / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+//                
+//                return v;
+//            }  
+//                
+//        }
+//        
+//        @Override
+//        public String toString(){return x + "," + y;}
+//        
+//    }
+//
 public class Main {
+//
+    public static void main(String[] args) throws IOException {
+//        
+//        
+//        BufferedImage img = ImageIO.read(new File("E:\\Projects\\Projects 2020\\ANN-Final Project\\op-charmander\\Data\\train\\allow.png"));
+//        int pix[][]= new int[img.getWidth()][img.getHeight()];
+//        
+//        for (int i=0 ; i < img.getWidth() ; i++)
+//            for (int j =0 ; j < img.getHeight() ; j++)
+//                pix[i][j]= img.getRGB(i,j);
+//        
+////        for (int i=0 ; i < img.getWidth() ; i++)
+////            for (int j =0 ; j < img.getHeight() ; j++)
+////                System.out.println (pix[i][j]);
+//        
+//        double sumx = 0;
+//        double sumy = 0;
+//
+//        for (int i=0 ; i < img.getWidth() ; i++){
+//            for (int j =0 ; j < img.getHeight() ; j++){
+//                
+//                if ( pix[i][j] == -1 ){
+//                    vector vec = new vector(i-14 , j-14);
+//                    vector ven = vec.normal();
+//                    sumx += ven.x;
+//                    sumy += ven.y;
+//                }
+//            
+//            }    
+//        }
+//        
+//        vector vec = new vector(sumx,sumy);
+//        vector vecn = vec.normal();
+//        System.out.println(vecn);
+        
+        
+        
+        
 
-
-    public static void main(String[] args) {
-        System.out.println("Operation Charmander Begins Now");
     }
     
 }
