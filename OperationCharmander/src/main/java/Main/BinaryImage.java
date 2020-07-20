3b0155b5f76ee2327baafc712a7619aba73ba5bb
Hadi Najafi <hadinajafi.77@gmail.com>
2020-07-20 10:28:50 +0430

    added return binary label and binary pixels

--- OperationCharmander/src/main/java/Main/BinaryImage.java
+++ OperationCharmander/src/main/java/Main/BinaryImage.java
@@ -3,19 +3,42 @@ package Main;
 import java.util.ArrayList;
 
 public class BinaryImage {
-    private double [][] binaryPixels;   //the matrix of pixels, if pixel color is white (255, 255, 255) then it's 1, and if it's black (0,0,0) then the number is 0;
-    private double [][] rgbPixels;
-    private ArrayList<Double[]> binaryImage = new ArrayList<>();
-    private int label;
-    private double[] binaryLabel;
-    
-    public BinaryImage(int label, double [][] binaryMatrix){
-        this.label = label;
-        binaryLabel = new double[10];
-        setBinaryPixels(binaryMatrix);
-    }
-    
-    public void setBinaryPixels(double [][] matrix){
-        this.binaryPixels = matrix;
-    }
+
+	private double[][] binaryPixels;   //the matrix of pixels, if pixel color is white (255, 255, 255) then it's 1, and if it's black (0,0,0) then the number is 0;
+	private double[][] rgbPixels;		//the matrix of RGB pixels of every pixel. for now it's not used & initialized.
+//    private ArrayList<double[]> binaryImage = new ArrayList<>();
+	private int label;
+	private double[] binaryLabel;
+
+	public BinaryImage(int label, double[][] binaryMatrix, double[] binaryLabel) {
+		this.label = label;
+		this.binaryLabel = binaryLabel;
+		setBinaryPixels(binaryMatrix);
+	}
+
+	public void setBinaryPixels(double[][] matrix) {
+		this.binaryPixels = matrix;
+	}
+
+	public double[][] getBinaryPixels() {
+		return this.binaryPixels;
+	}
+
+	public double[] getBinaryLabel() {
+		return this.binaryLabel;
+	}
+
+	public double[] getContinuousBinaryPixels() {
+		int w = this.binaryPixels.length;
+		int h = this.binaryPixels[0].length;
+		double[] continuousBinPixels = new double[w * h];
+		int iterator = 0;
+		for (int i = 0; i < w; i++) {
+			for (int j = 0; j < h; j++) {
+				continuousBinPixels[iterator] = binaryPixels[i][j];
+				iterator++;
+			}
+		}
+		return continuousBinPixels;
+	}
 }
